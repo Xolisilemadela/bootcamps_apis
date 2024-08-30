7ac5ae8db9169b90f84aa5d4020271051661c158
Xolisile madela <your email here>
2024-08-30 17:00:58 +0200

    first commit

---
+++ Bootcamp/wordLengths.js
@@ -0,0 +1,9 @@
+
+export default function wordLengths(sentence){
+    var sentenceList = sentence.split(" ");
+    var count = 0;
+    for(let i=0; i<sentenceList.length; i++){
+        count += sentenceList[i].length;
+    }
+    return count;
+}
